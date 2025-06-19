4d6e08034d35d19c542cf8d00a606f6b54b557da
mfreemon@cloudflare.com <mfreemon@cloudflare.com>
2025-06-19 16:36:02 -0500

    version 0.0.22

--- src/bbperf/const.py
+++ src/bbperf/const.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024 Cloudflare, Inc.
 # Licensed under the Apache 2.0 license found in the LICENSE file or at https://www.apache.org/licenses/LICENSE-2.0
 
-BBPERF_VERSION = "0.0.21"
+BBPERF_VERSION = "0.0.22"
 
 SERVER_PORT = 5301
 
